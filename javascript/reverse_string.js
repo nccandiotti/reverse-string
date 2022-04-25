fb43ae1afea2ec7df10fec873d18e2b0a481dbad
Nicole Candiotti <nccandiotti@gmail.com>
2022-04-25 19:04:44 -0400

    commit tree 63f406908846462e877a88c35a8c27dd07bbe118

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,19 +1,25 @@
 function reverseString(str) {
   // type your code here
+  const arr = str.split("")
+  for (i = arr.length - 1; i >= 0 - 1; i--) {
+    console.log(arr[i])
+  }
 }
 
+reverseString("fish")
+reverseString("hi")
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 'ih'");
-  console.log("=>", reverseString("hi"));
+  console.log("Expecting: 'ih'")
+  console.log("=>", reverseString("hi"))
 
-  console.log("");
+  console.log("")
 
-  console.log("Expecting: 'ybabtac'");
-  console.log("=>", reverseString("catbaby"));
+  console.log("Expecting: 'ybabtac'")
+  console.log("=>", reverseString("catbaby"))
 }
 
-module.exports = reverseString;
+module.exports = reverseString
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
